f77708b5584374f97f3e4885563fcf499bdd3e2c
Bryan Lincoln <bryanufg@gmail.com>
2024-03-30 10:24:16 -0300

    fix(daemon): update relative path

--- experiments/daemon.sh
+++ experiments/daemon.sh
@@ -4,13 +4,13 @@ mkdir -p 1.to_run 2.queued 3.running 4.executed
 
 
 while true; do
-  # Check if there are any scripts in the "./to_run" folder
-  if [ -n "$(ls -A ./1.to_run)" ]; then
+  # Check if there are any scripts in the "experiments/to_run" folder
+  if [ -n "$(ls -A experiments/1.to_run)" ]; then
     # Get the first script in the "experiments/1.to_run" folder
-    script=$(ls ./1.to_run | head -n 1)
+    script=$(ls experiments/1.to_run | head -n 1)
 
     echo "---- Moving $script to 2.queued"
-    mv "./1.to_run/$script" "./2.queued/$script"
+    mv "experiments/1.to_run/$script" "experiments/2.queued/$script"
     echo "---- Queued $script"
 
     # Check if there is at least 30% of RAM available
@@ -35,15 +35,15 @@ while true; do
     done
 
     echo "---- Moving $script to 3.running"
-    mv "./2.queued/$script" "./3.running/$script"
+    mv "experiments/2.queued/$script" "experiments/3.running/$script"
 
     # Execute the script
-    source "./3.running/$script"
+    source "experiments/3.running/$script"
 
     echo "---- Moving $script to 4.executed"
-    mv "./3.running/$script" "./4.executed/$script"
+    mv "experiments/3.running/$script" "experiments/4.executed/$script"
   else
-    # If there are no scripts in the "./1.to_run" folder, print current timestamp and sleep
+    # If there are no scripts in the "experiments/1.to_run" folder, print current timestamp and sleep
     echo "---- No scripts to run"
     date
     sleep 10
