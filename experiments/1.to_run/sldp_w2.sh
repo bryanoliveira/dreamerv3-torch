5eabda9c7f27d877722ae7690262e60b82e294bf
Bryan Lincoln <bryanufg@gmail.com>
2024-03-30 10:42:36 -0300

    chore: update experiment size

--- experiments/1.to_run/sldp_w2.sh
+++ experiments/1.to_run/sldp_w2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_onehot_w2 --configs sldp --seed 176 --batch_size 64 --train_ratio 4096
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_onehot_w2 --configs sldp --seed 568 --batch_size 64 --train_ratio 4096
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_onehot_w2 --configs sldp --seed 907 --batch_size 64 --train_ratio 4096
+python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_onehot_w2 --configs sldp --seed 176 --batch_size 48 --train_ratio 3072
+python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_onehot_w2 --configs sldp --seed 568 --batch_size 48 --train_ratio 3072
+python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_onehot_w2 --configs sldp --seed 907 --batch_size 48 --train_ratio 3072
 # python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_single_w2 --configs sldp_image --seed 467
 # python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_imagenet_w2 --configs sldp_image --seed 467 --env__image_folder /mnt/data/Documents/sliding-puzzle/imgs/imagenet-1k
 # python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-sldp_mnist_w2 --configs sldp_image --seed 467 --env__image_folder /mnt/data/Documents/sliding-puzzle/imgs/mnist
