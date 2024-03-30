0c34f87b79ce3d836b25239ef18382dc9ac74a93
Bryan Lincoln <bryanufg@gmail.com>
2024-03-30 11:10:19 -0300

    fix: update experiment difficulties

--- experiments/3.running/sldp_w3_bp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w3_b96 --configs sldp --seed 568 --batch_size 80 --train_ratio 5120
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w3_b96 --configs sldp --seed 907 --batch_size 80 --train_ratio 5120
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w3_b96 --configs sldp --seed 176 --batch_size 80 --train_ratio 5120
-# python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-single_w3_b96 --configs sldp_image --seed 176
-# python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-imagenet_w3_b96 --configs sldp_image --seed 176 --env__image_folder /mnt/data/Documents/sliding-puzzle/imgs/imagenet-1k
-# python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-mnist_w3_b96 --configs sldp_image --seed 176 --env__image_folder /mnt/data/Documents/sliding-puzzle/imgs/mnist
--- experiments/3.running/sldp_w5.sh
+++ experiments/3.running/sldp_w5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w3 --configs sldp --seed 176 --batch_size 48 --train_ratio 3072 --env__w 3
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w3 --configs sldp --seed 568 --batch_size 48 --train_ratio 3072 --env__w 3
-python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w3 --configs sldp --seed 907 --batch_size 48 --train_ratio 3072 --env__w 3
+python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w5 --configs sldp --seed 176 --batch_size 48 --train_ratio 3072 --env__w 5
+python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w5 --configs sldp --seed 568 --batch_size 48 --train_ratio 3072 --env__w 5
+python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-onehot_w5 --configs sldp --seed 907 --batch_size 48 --train_ratio 3072 --env__w 5
 # python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-single_w3 --configs sldp_image --seed 176
 # python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-imagenet_w3 --configs sldp_image --seed 176 --env__image_folder /mnt/data/Documents/sliding-puzzle/imgs/imagenet-1k
 # python dreamer.py --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-mnist_w3 --configs sldp_image --seed 176 --env__image_folder /mnt/data/Documents/sliding-puzzle/imgs/mnist
